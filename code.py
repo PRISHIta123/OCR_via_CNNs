81a374a915ed3628fdbc373cc8e1f5caba57619b
Prishita Ray <30746562+PRISHIta123@users.noreply.github.com>
2018-09-28 17:36:21 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,14 @@
+import sys
+sys.path
+import web
+urls = (
+'/input', 'index'
+)
+class index:
+    def GET(self):
+        i = web.input(name=None)
+        return render.index(i.name)
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
+render = web.template.render('templates/')
